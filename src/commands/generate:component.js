387c4859e178b5124c94a5b5a30c24e7b6edf0df
Bruno Simplicio <brunoraphael89@gmail.com>
2019-05-28 21:39:03 -0300

    Initial commit

---
+++ src/commands/generate:component.js
@@ -0,0 +1,31 @@
+module.exports = {
+  name: 'generate:component',
+  description: 'Create new component inside src/components',
+  run: async toolbox => {
+    const {
+      parameters,
+      template,
+      print: { success, error }
+    } = toolbox
+
+    const name = parameters.first
+
+    if (!name) {
+      error('Component name must be specified')
+      return
+    }
+
+    await template.generate({
+      template: 'component.js.ejs',
+      target: `src/components/${name}/index.js`,
+      props: { name: name }
+    })
+
+    await template.generate({
+      template: 'styles.js.ejs',
+      target: `src/components/${name}/styles.js`
+    })
+
+    success(`Generated ${name} component.`)
+  }
+}
